06256f48f1f12ffc73067ec936c9dfc5dca3a3da
Trần Gia Vĩ <127858226+gitcuaVi@users.noreply.github.com>
2025-02-28 10:37:05 +0700

    test err

--- h-app/src/pages/dashboard/index.jsx
+++ h-app/src/pages/dashboard/index.jsx
@@ -102,25 +102,22 @@ const WorkHistory = () => (
 
 
 const Dashboard = () => {
-  throw new Error("Lỗi test: Dashboard bị lỗi!");
-
   return (
     <div className="container">
       <div className="content-profile">
-        <Profile />
+      <Profile />
       </div>
       <div className="content">
         <div className="section-title-week">Lịch tuần - tháng 2 2025</div>
         <CalendarWeek />
       </div>
-      <div className="section-title">Truy cập nhanh</div>
-      <QuickAccess />
+        <div className="section-title">Truy cập nhanh</div>
+        <QuickAccess />
       <div className="content-history">
         <WorkHistory />
-      </div>
+      </div>  
     </div>
   );
 };
-
 
 export default Dashboard;
