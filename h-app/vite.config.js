042fca553bc6b58ab8c65ed47614c9ab5002e525
Trần Gia Vĩ <127858226+gitcuaVi@users.noreply.github.com>
2025-03-03 16:29:11 +0700

    wwww

--- h-app/vite.config.js
+++ h-app/vite.config.js
@@ -34,7 +34,7 @@ export default defineConfig({
     include: ['antd', '@ant-design/icons'],
     exclude: ['some-server-only-package'],
   },
-
-
-
+  esbuild: {
+    jsxInject: `import React from 'react'`,
+  },
 });
