f4bd4bd28c2e8518edfdab0debda3a28220d6bdd
Trần Gia Vĩ <127858226+gitcuaVi@users.noreply.github.com>
2025-03-03 16:28:01 +0700

    as

--- h-app/vite.config.js
+++ h-app/vite.config.js
@@ -1,53 +1,40 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 import path from 'path';
-import { fileURLToPath } from 'url';
-
-// Fix lỗi __dirname khi dùng ES module
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 export default defineConfig({
-  plugins: [react()],
-  base: "./", 
-
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
+  base: "./",
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
     },
   },
-
   server: {
     port: 3000,
     host: true,
   },
-
   build: {
-    target: 'esnext',
-    chunkSizeWarningLimit: 1000,
-
     rollupOptions: {
       output: {
-
         manualChunks(id) {
-          if (id.includes('node_modules')) {
-            if (id.includes('antd')) return 'antd';
-            if (id.includes('react')) return 'react-vendor';
-            return 'vendor';
+          if (id.includes('node_modules/antd')) {
+            return 'antd'; // Create a separate chunk for antd
           }
         },
       },
     },
+    chunkSizeWarningLimit: 1000, // Increase chunk size limit
+  },
+  optimizeDeps: {
+    include: ['antd', '@ant-design/icons'],
+    exclude: ['some-server-only-package'],
   },
 
-  optimizeDeps: {
-    esbuildOptions: {
-      target: 'esnext',
-      loader: {
-        '.js': 'jsx',
-        '.ts': 'tsx',
-      },
-    },
-  },
-  
+
+
 });
