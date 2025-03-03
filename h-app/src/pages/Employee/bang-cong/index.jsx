4d8587e9a6bc3701e6413f30e186264e6dadfadc
Trần Gia Vĩ <127858226+gitcuaVi@users.noreply.github.com>
2025-03-03 14:28:50 +0700

    a

--- h-app/src/pages/Employee/bang-cong/index.jsx
+++ h-app/src/pages/Employee/bang-cong/index.jsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React, { useEffect, useState } from "react";
 import { Select } from "@/components/ui/select";
 import { Skeleton } from "@/components/ui/skeleton";
--- h-app/src/pages/Employee/tang-ca/index.jsx
+++ h-app/src/pages/Employee/tang-ca/index.jsx
@@ -1,3 +1,6 @@
+"use client";
+
+
 import React from "react";
 import { Alert } from "@/components/ui/alert";
 import { Skeleton } from "@/components/ui/skeleton";
