c2e8a89f1940bbc8b658b1ed7daa62cc84ed2e14
Trần Gia Vĩ <127858226+gitcuaVi@users.noreply.github.com>
2025-03-05 11:40:21 +0700

    addd

--- h-app/src/pages/dashboard/profile/index.jsx
+++ h-app/src/pages/dashboard/profile/index.jsx
@@ -1,34 +1,34 @@
 import React, { useState, useEffect } from "react";
-import  {fetchTelegramUserData}  from "@/store/slice/profileidSlice";
 import profileImg from "@/assets/profile.jpg";
 
 const Profile = () => {
   const [user, setUser] = useState({
     id: null,
-    name: "Đinh Hoàng Lượm",
-    location: "BL-HCM",
-    username: "",
+    name: "Đang lấy...",
+    username: "Đang lấy...",
   });
 
   useEffect(() => {
-    const getUserData = async () => {
-      const userData = await fetchTelegramUserData();
-      if (userData) {
-        setUser((prevUser) => ({
-          ...prevUser,
-          ...userData,
-        }));
-      }
-    };
+    if (window.Telegram && window.Telegram.WebApp) {
+      const tg = window.Telegram.WebApp;
+      const userData = tg.initDataUnsafe.user;
 
-    getUserData();
+      if (userData) {
+        setUser({
+          id: userData.id,
+          name: `${userData.first_name} ${userData.last_name || ""}`,
+          username: userData.username || "Không có username",
+        });
+      } else {
+        console.log("❌ Không thể lấy dữ liệu người dùng từ Telegram");
+      }
+    }
   }, []);
 
   return (
     <div className="profile">
       <img src={profileImg} alt="Profile" className="profile-img" />
       <div className="name">Tên: {user.name}</div>
-      <div className="location">Địa điểm: {user.location}</div>
       <div className="username">Username: {user.username}</div>
       <div className="user-id">Telegram ID: {user.id || "Đang lấy..."}</div>
     </div>
